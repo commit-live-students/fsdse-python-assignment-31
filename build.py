21a5252e7afeb0c48a11c7183a3cdc41b72f45b4
shunya-mandal <100ameya@gmail.com>
2017-05-30 19:17:10 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools, operator
 
 
 def solution(dic):
@@ -6,4 +6,15 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    letter_groups = list(sorted(dic.values()))
+    for i in range(len(letter_groups)-1):
+        for j in range(i+1, len(letter_groups)):
+            l1, l2 = letter_groups[i], letter_groups[j]
+            g = [list(zip(x, l2)) for x in list(itertools.permutations(l1, len(l2)))]
+            pairs = list(set(['{}{}'.format(x[0],x[1]) for x in reduce(operator.concat, g)]))
+            list_of_combinations.append(pairs)
+    list_of_combinations = sorted(reduce(operator.concat, list_of_combinations))
     return list_of_combinations
+
+d = {'one':['a','b'], 'two':['c','d']}
+print(solution(d))
