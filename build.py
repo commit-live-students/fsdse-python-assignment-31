109ec57aea0872398994d70f0cd88f94167ba7b3
rishabh-github <rishabh-github@users.noreply.github.com>
2017-06-03 10:55:10 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 import itertools
 
-
+list_of_combinations = {}
 def solution(dic):
-    list_of_combinations = []
-    '''
-    Enter your code here
-    '''
+    print sorted(dic.keys())
+    print type(sorted(dic.keys()))
+
+    values = [v[1] for v in sorted(dic.items())]
+    list_of_combinations = [''.join(x) for x in itertools.product(*values)]
+
     return list_of_combinations
+
+print solution({'one':['a','b'], 'two':['c','d']})
