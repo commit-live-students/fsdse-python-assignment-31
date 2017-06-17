5781dde6fab56529f87da5643cc1f57099fb1285
harishkandan <harishkandan95@gmail.com>
2017-06-17 03:36:03 +0000

    Done

--- build.py
+++ build.py
@@ -3,7 +3,10 @@ import itertools
 
 def solution(dic):
     list_of_combinations = []
-    '''
-    Enter your code here
-    '''
+    onevalues=dic['one']
+    twovalues=dic['two']
+    for i in range(0,len(onevalues)):
+        for j in range(0,len(twovalues)):
+            list_of_combinations.append(onevalues[i]+twovalues[j])
     return list_of_combinations
+print solution({'one':['a','b'], 'two':['c','d']})
