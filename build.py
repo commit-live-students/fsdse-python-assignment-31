b1377e5bf15ff57b23e93b057563cbeedc926d4d
adinathauti <adinathauti@gmail.com>
2017-06-19 08:57:14 +0000

    Done

--- build.py
+++ build.py
@@ -5,10 +5,6 @@ def solution(dic):
     lst = []
     tmp = dic.values()
     tmp.sort()
-    print tmp
-    tmp2 = []
-    k = 0
-    print tmp
     for i in range(len(tmp)):
         for k in range(len(tmp)):
             lst.append(tmp[0][i]+tmp[1][k])
