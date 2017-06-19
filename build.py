4668ce5b26b0c0e3a5e59ad359161b9bacc9d36d
bhattbhavesh91 <bhattbhavesh91@gmail.com>
2017-06-19 16:13:45 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,26 @@
+"""
+Write a Python program to create and display all combinations of letters, selecting each letter from a different key in a dictionary.
+Notes:
+
+Define a function which will accept a dict parameter.
+For all present keys it will fetch values and will create a pair as described in above example.
+Store it in list and return.
+Instructions:
+
+Program should be written in file build.py
+
+Function name should be solution.
+Input
+ Type:  Dictionary
+ Value: {'one':['a','b'], 'two':['c','d']}
+Expected Output
+  Type:  List
+  Value: [ac, ad, bc, bd]
+"""
 import itertools
-
-
 def solution(dic):
     list_of_combinations = []
-    '''
-    Enter your code here
-    '''
+    dic1 = dic['one']
+    dic2 = dic['two']
+    list_of_combinations = [dic1[i]+dic2[j] for i in range(0,len(dic1)) for j in range (0,len(dic2))]
     return list_of_combinations
