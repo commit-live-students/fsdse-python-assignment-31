57557b28e64dfb041e06d04f9b28d1cb39cf23f8
nitikagoel01 <nitikagoel.1693@gmail.com>
2017-06-21 19:07:38 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,7 @@
 import itertools
-
-
 def solution(dic):
     list_of_combinations = []
-    '''
-    Enter your code here
-    '''
+    dic1 = dic['one']
+    dic2 = dic['two']
+    list_of_combinations = [dic1[i]+dic2[j] for i in range(0,len(dic1)) for j in range (0,len(dic2))]
     return list_of_combinations
