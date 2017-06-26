858b8500cca20d6eb23abac844f568416531a694
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-26 15:11:55 +0000

    Done

--- build.py
+++ build.py
@@ -3,7 +3,10 @@ import itertools
 
 def solution(dic):
     list_of_combinations = []
-    '''
-    Enter your code here
-    '''
+    for combo in itertools.product(*[dic[k] for k in sorted(dic.keys())]):
+        val = (''.join(combo))
+        list_of_combinations.append(val)
     return list_of_combinations
+
+my_dict = {'1':['a','b'], '2':['c','d']}
+solution(my_dict)
